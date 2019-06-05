2770ccc6eda54e31ef84c9393a69e881ecca7ce5
Rajat Mangal <rmangal@pnimedia.com>
2019-06-05 16:34:10 -0700

    button added

--- lib/main.dart
+++ lib/main.dart
@@ -22,14 +22,18 @@ class DicePage extends StatelessWidget {
       child: Row(
         children: <Widget>[
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
+            child: FlatButton(
+              onPressed: () {
+                print("Left Button Pressed");
+              },
               child: Image.asset("images/dice1.png"),
             ),
           ),
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
+            child: FlatButton(
+              onPressed: () {
+                print("Right Button Pressed");
+              },
               child: Image.asset("images/dice1.png"),
             ),
           ),
